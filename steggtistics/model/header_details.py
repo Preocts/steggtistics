c4847d48aac379c1655007e705d1997fb54a5e70
Preocts <preocts@gmail.com>
2022-03-11 20:45:53 -0500

    Flake8 updates

--- steggtistics/model/header_details.py
+++ steggtistics/model/header_details.py
@@ -1,3 +1,4 @@
+"""Model HTTP response from GitHub API."""
 from __future__ import annotations
 
 import re
@@ -5,23 +6,25 @@ from datetime import datetime
 
 
 class HeaderDetails:
-    next: str | None
-    prev: str | None
-    last: str | None
-    first: str | None
+    """Model HTTP response from GitHub API."""
+
+    next_url: str | None
+    prev_url: str | None
+    last_url: str | None
+    first_url: str | None
     total: int
     remaining: int
     rate_reset: datetime
 
     @classmethod
     def build_from(cls, httpdict: dict[str, str]) -> HeaderDetails:
-        """Build model from HTTP response"""
+        """Build model from HTTP response."""
         newobj = cls()
-        newobj.next = cls._extract_next(httpdict["Link"], "next")
-        newobj.prev = cls._extract_next(httpdict["Link"], "prev")
-        newobj.last = cls._extract_next(httpdict["Link"], "last")
-        newobj.first = cls._extract_next(httpdict["Link"], "first")
-        newobj.total = cls._extract_total(newobj.last)
+        newobj.next_url = cls._extract_next(httpdict["Link"], "next")
+        newobj.prev_url = cls._extract_next(httpdict["Link"], "prev")
+        newobj.last_url = cls._extract_next(httpdict["Link"], "last")
+        newobj.first_url = cls._extract_next(httpdict["Link"], "first")
+        newobj.total = cls._extract_total(newobj.last_url)
         newobj.remaining = int(httpdict["X-RateLimit-Remaining"])
         newobj.rate_reset = datetime.fromtimestamp(float(httpdict["X-RateLimit-Reset"]))
 
@@ -29,7 +32,7 @@ class HeaderDetails:
 
     @staticmethod
     def _extract_next(link: str, rel: str) -> str | None:
-        """internal: extract next url from headers.link"""
+        """Extract next url from headers.link."""
         pattern = rf'<(.*?)>; rel="{rel}"'
         for link_seg in link.split(","):
             match = re.match(pattern, link_seg.strip())
@@ -39,7 +42,7 @@ class HeaderDetails:
 
     @staticmethod
     def _extract_total(last: str | None) -> int:
-        """internal: extract total pages from last link or return 0"""
+        """Extract total pages from last link or return 0."""
         pattern = r"(?<!per_)page=(\d+)"
         match = re.search(pattern, last or "")
         return int(match.group(1)) if match else 0
