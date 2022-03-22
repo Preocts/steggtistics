95bacb2d412a8287e612db0bdb184a3ecbda36ec
Preocts <preocts@gmail.com>
2022-03-21 22:21:53 -0400

    Read/Write methods for datastore

--- steggtistics/data_store.py
+++ steggtistics/data_store.py
@@ -2,7 +2,11 @@
 from __future__ import annotations
 
 import sqlite3
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Generator
+
+from steggtistics.model.event import Event
 
 
 class DataStore:
@@ -17,12 +21,49 @@ class DataStore:
 
     def _build_table(self) -> None:
         """Build table if doesn't exist."""
-        cursor = self._db.cursor()
-        try:
+        with self._cursor() as cursor:
             cursor.execute(
-                "CREATE TABLE IF NOT EXISTS events(id_ TEXT, type_ TEXT, "
+                "CREATE TABLE IF NOT EXISTS events(id TEXT, type TEXT, "
                 "public TEXT, created_at TEXT, repo_name TEXT, repo_url TEXT)"
             )
             self._db.commit()
+
+    def row_count(self) -> int:
+        """Return total number of rows in loaded database file."""
+        with self._cursor() as cursor:
+            cursor.execute("SELECT COUNT(id) FROM events")
+            return cursor.fetchone()[0]
+
+    def save_row(self, row: Event) -> None:
+        """Single save row to database."""
+        self.save_rows([row])
+
+    def save_rows(self, rows: list[Event]) -> None:
+        """Bulk save rows to database."""
+        # NOTE: Insert order must match Event model
+        sql = (
+            "INSERT INTO events (id, type, public, created_at, repo_name, repo_url) "
+            "VALUES (?,?,?,?,?,?)"
+        )
+
+        with self._cursor() as cursor:
+            for row in rows:
+                cursor.execute(sql, tuple(row.asdict().values()))
+            self._db.commit()
+
+    def get_rows(self) -> list[Event]:
+        """Fetch all rows from database."""
+        with self._cursor() as cursor:
+            cursor.execute("SELECT * FROM events")
+            results = cursor.fetchall()
+
+        return [Event.build_from_row(*result) for result in results]
+
+    @contextmanager
+    def _cursor(self) -> Generator[sqlite3.Cursor, None, None]:
+        """Context manager to ensure cursors are closed."""
+        try:
+            cursor = self._db.cursor()
+            yield cursor
         finally:
             cursor.close()
--- tests/data_store_test.py
+++ tests/data_store_test.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 from sqlite3 import Connection
 from tempfile import mkstemp
@@ -6,10 +8,12 @@ from typing import Generator
 import pytest
 
 from steggtistics.data_store import DataStore
+from steggtistics.model.event import Event
 
+NUMBER_OF_ROWS = 100  # For mock fill
 EXPECTED_COLUMNS = [
-    "id_",
-    "type_",
+    "id",
+    "type",
     "public",
     "created_at",
     "repo_name",
@@ -30,9 +34,30 @@ def store() -> Generator[DataStore, None, None]:
         os.remove(path)
 
 
+@pytest.fixture
+def mock_rows() -> list[Event]:
+    rows: list[Event] = []
+    for idx in range(NUMBER_OF_ROWS):
+        row = Event()
+        row.id_ = str(idx)
+        row.type_ = "mock"
+        row.public = True
+        row.created_at = "mock"
+        row.repo_name = "mock"
+        row.repo_url = "mock"
+        rows.append(row)
+    return rows
+
+
 def test_init(store: DataStore) -> None:
     assert os.path.isfile(store._path)
     assert isinstance(store._db, Connection)
+
+
+def test_get_size_empty(store: DataStore) -> None:
+    size = store.row_count()
+
+    assert size == 0
 
 
 def test_create_table(store: DataStore) -> None:
@@ -43,3 +68,23 @@ def test_create_table(store: DataStore) -> None:
     result = set(names) - set(EXPECTED_COLUMNS)
 
     assert len(result) == 0
+
+
+def test_add_rows(store: DataStore, mock_rows: list[Event]) -> None:
+    store.save_rows(mock_rows)
+
+    assert store.row_count() == NUMBER_OF_ROWS
+
+
+def test_add_row(store: DataStore, mock_rows: list[Event]) -> None:
+    store.save_row(mock_rows[0])
+
+    assert store.row_count() == 1
+
+
+def test_get_rows(store: DataStore, mock_rows: list[Event]) -> None:
+    store.save_rows(mock_rows)
+
+    results = store.get_rows()
+
+    assert len(results) == NUMBER_OF_ROWS
